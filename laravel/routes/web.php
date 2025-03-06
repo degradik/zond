8e02b74bf95f8470a2cac82c7fa7ef112b7b20b0
degradik <daniilzolotarev24052006@gmail.com>
2025-03-06 11:22:47 +0300

    +livewire

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\Counter;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+ 
+Route::get('/counter', Counter::class);
